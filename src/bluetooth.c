6eff00c06b5ca4edb2e1c31464e2b14a6ef9eeee
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-09-11 21:29:33 -0300

    chore: fix formatting

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -1,7 +1,6 @@
 #include "bt_kmn/bluetooth.h"
 #include "bt_kmn/commands.h"
 
-#include "stdbool.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 #include "hardware/uart.h"
@@ -13,26 +12,27 @@ uint8_t bt_buffer[MAX_COMMAND_SIZE];
 
 static BluetoothConfig _config;
 
-void Bluetooth_Setup(const BluetoothConfig config){
-    _config = config;
-    gpio_init(_config.rx_pin);
-    gpio_init(_config.tx_pin);
+void Bluetooth_Setup(const BluetoothConfig config) {
+  _config = config;
+  gpio_init(_config.rx_pin);
+  gpio_init(_config.tx_pin);
 
-    gpio_set_function(_config.rx_pin, GPIO_FUNC_UART);
-    gpio_set_function(_config.tx_pin, GPIO_FUNC_UART);
+  gpio_set_function(_config.rx_pin, GPIO_FUNC_UART);
+  gpio_set_function(_config.tx_pin, GPIO_FUNC_UART);
 
-    uart_init(_config.uart, _config.baudrate);
+  uart_init(_config.uart, _config.baudrate);
 }
 
-void Bluetooth_InterruptInit(irq_handler_t handler){
-    const int irq = _config.uart == uart0 ? UART0_IRQ : UART1_IRQ;
-    irq_set_exclusive_handler(irq, handler);
-    irq_set_enabled(irq, true);
+void Bluetooth_InterruptInit(irq_handler_t handler) {
+  const int irq = _config.uart == uart0 ? UART0_IRQ : UART1_IRQ;
+  irq_set_exclusive_handler(irq, handler);
+  irq_set_enabled(irq, true);
 }
 
-void Bluetooth_AtCommand(const char* command){
-    uart_puts(_config.uart, command);
-    uart_puts(_config.uart, "\r\n");
+void Bluetooth_AtCommand(const char *command) {
+  uart_puts(_config.uart, command);
+  uart_puts(_config.uart, "\r\n");
+}
 
 void Bluetooth_SendMessage(const char *message) {
   uart_puts(_config.uart, message);

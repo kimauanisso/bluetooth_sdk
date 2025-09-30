a9a23cf4861fb454eab0f166588e4ec55f4dab32
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-09-30 20:17:25 -0300

    fix: add missing pragma once

--- include/bt_kmn/bluetooth.h
+++ include/bt_kmn/bluetooth.h
@@ -1,17 +1,19 @@
-#include "hardware/uart.h"
+#pragma once
+
 #include "hardware/irq.h"
+#include "hardware/uart.h"
 
 typedef struct BluetoothConfig {
-    uart_inst_t* uart;
-    uint baudrate;
-    uint rx_pin;
-    uint tx_pin;
+  uart_inst_t *uart;
+  uint baudrate;
+  uint rx_pin;
+  uint tx_pin;
 } BluetoothConfig;
 
 void Bluetooth_Setup(BluetoothConfig config);
 void Bluetooth_InterruptInit(irq_handler_t handler);
 
-void Bluetooth_AtCommand(const char* command);
+void Bluetooth_AtCommand(const char *command);
 
 void Bluetooth_SendMessage(const char *message);
 void Bluetooth_ReadMessage();
